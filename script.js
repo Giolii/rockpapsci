d0b1ce0ed2ee65d4ce1f0a68df9335106e8de23e
Gi <160989051+Giolii@users.noreply.github.com>
2024-04-18 09:14:15 -0700

    Completed function for a one round pplay

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-//Computer picks rokc paper or scissor
-let choices = ['rock', 'paper', 'scissor']
-let compChoice = '';
-function getComputerChoice(choices) {
- compChoice = choices[Math.floor(Math.random() * choices.length)];
-}
+//Computer picks rock, paper or scissor
 
+function getComputerChoice() {
+    let choices = ['rock', 'paper', 'scissor'];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+// One Round
+
+function playRound(playerSelection,computerSelection){
+    let tie = `TIE! ${playerSelection} and ${computerSelection}`;
+    let win = `You win, ${playerSelection} beats ${computerSelection}.`;
+    let lose= `You lose, ${computerSelection} beats ${playerSelection}.`;
+
+    if ((playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'scissor') ||
+    (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'rock') || 
+    (playerSelection.toLowerCase() === 'scissor' && computerSelection.toLowerCase() === 'paper')){
+        return win;
+    } else if (playerSelection.toLowerCase() === computerSelection.toLowerCase()){
+           return tie;
+    } else {
+        return lose;
+    }
+    }
+
+    const playerSelection = "rock";
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
