ce3ab45469e188882a790174374c2439840b313b
Gi <160989051+Giolii@users.noreply.github.com>
2024-04-18 21:43:56 -0700

    New icons, buttons connected with div

--- script.js
+++ script.js
@@ -9,32 +9,7 @@ function getComputerChoice() { //Computer picks rock, paper or scissor
 
 
 function playGame(){ //Play 5 games
-    for (let i = 0; ; ) {
-        const playerSelection = prompt('Rock, paper or scissor?');
-        const computerSelection = getComputerChoice();
         const result = playRound(playerSelection, computerSelection);
-        function playRound(playerSelection,computerSelection){
-            let tie = `TIE! ${playerSelection} and ${computerSelection}`;
-            let win = `Yes, ${playerSelection} beats ${computerSelection}.`;
-            let lose= `No, ${computerSelection} beats ${playerSelection}.`;
-        
-            if ((playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'scissor') ||
-            (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'rock') || 
-            (playerSelection.toLowerCase() === 'scissor' && computerSelection.toLowerCase() === 'paper')){
-                player++;
-                i++;
-                return win;
-            } else if (playerSelection.toLowerCase() === computerSelection.toLowerCase()){
-                   return tie;
-            } else if ((playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'paper') ||
-            (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'scissor') || 
-            (playerSelection.toLowerCase() === 'scissor' && computerSelection.toLowerCase() === 'rock')){
-                mac++;
-                i++;
-                return lose;}
-            else { 
-                return mistake;}
-            }
             console.log(result); // Log the result to the console
         if (mac + player === 5 && mac > player) {
             console.log(`You lose! Mac points:${mac}, Player points:${player}`);
@@ -43,15 +18,48 @@ function playGame(){ //Play 5 games
             console.log(`You win! Player points:${player}, Mac points:${mac}.`);
             // break;
         }
-
-    }
 }
-// playGame(); 
+
+function playRound(playerSelection,computerSelection){
+    let tie = `TIE! ${playerSelection} and ${computerSelection}`;
+    let win = `Yes, ${playerSelection} beats ${computerSelection}.`;
+    let lose= `No, ${computerSelection} beats ${playerSelection}.`;
+
+    if ((playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'scissor') ||
+    (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'rock') || 
+    (playerSelection.toLowerCase() === 'scissor' && computerSelection.toLowerCase() === 'paper')){
+        player++;
+        // i++;
+        return win;
+    } else if (playerSelection.toLowerCase() === computerSelection.toLowerCase()){
+           return tie;
+    } else if ((playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'paper') ||
+    (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'scissor') || 
+    (playerSelection.toLowerCase() === 'scissor' && computerSelection.toLowerCase() === 'rock')){
+        mac++;
+        // i++;
+        return lose;}
+    else { 
+        return mistake;}
+    }
 
 const btnRock = document.querySelector('#rock');
 const btnPaper = document.querySelector('#paper');
 const btnScissor = document.querySelector('#scissor');
+const result = document.querySelector('#result');
 
-btnRock.addEventListener('click', () => console.log("clicked"));
-btnPaper.addEventListener('click', () => console.log("clicked"));
-btnScissor.addEventListener('click', () => console.log("clicked"));
+btnRock.addEventListener('click', () => {
+    playerSelection = 'rock';
+    const computerSelection = getComputerChoice();
+    result.textContent = (playRound(playerSelection,computerSelection));
+})
+btnPaper.addEventListener('click', () => {
+    playerSelection = 'paper';
+    const computerSelection = getComputerChoice();
+    result.textContent = (playRound(playerSelection,computerSelection));
+})
+btnScissor.addEventListener('click', () => {
+    playerSelection = 'scissor';
+    const computerSelection = getComputerChoice();
+    result.textContent = (playRound(playerSelection,computerSelection));
+})
